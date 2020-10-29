c3a38b32e5f7633fe4578a177f5ab7e6f1eae44c
Stephen Tan <sttan@ucsd.edu>
2020-10-29 12:21:22 -0700

    Added tweet table view cell for individual tweets

--- Twitter/ViewControllers/HomeTableViewController.swift
+++ Twitter/ViewControllers/HomeTableViewController.swift
@@ -25,7 +25,7 @@ class HomeTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,14 +33,14 @@ class HomeTableViewController: UITableViewController {
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetTableViewCell
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetContentLabel.text = "Some text"
 
         return cell
     }
-    */
+    
 
 }
