6b2df0b79abcb9e2026b5c9b8e04666f02c2bada
Stephen Tan <sttan@ucsd.edu>
2020-10-29 11:55:44 -0700

    Implemented UserDefaults for user login/logout

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -14,10 +14,18 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        // do something after page shows up
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let myUrl = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not login")
